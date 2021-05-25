e322c9700a8d2cdefaef792edae7909695c8efdb
Lili Beit <lilisbeit@gmail.com>
2021-05-25 14:33:05 -0400

    kfold function

---
+++ utils.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.metrics import plot_confusion_matrix, accuracy_score
+from sklearn.metrics import recall_score, precision_score, f1_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import ComplementNB, MultinomialNB
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+def k_fold_validator(predictor, target, vectorizer, classifier, cv=5):
+
+    kf = KFold(n_splits=cv)
+    vec = vectorizer
+    clf = classifier
+
+    train_recall_scores = []
+    train_precision_scores = []
+    train_f1_scores = []
+    test_recall_scores = []
+    test_precision_scores = []
+    test_f1_scores = []
+    
+    print('Vectorizer:', vectorizer)
+    print('Classifier:', clf)
+    print('Cross-validation folds:', cv)
+    
+    for train_index, test_index in kf.split(X):
+
+        X_tr, X_test = X[train_index].astype(str), X[test_index].astype(str)
+        y_tr, y_test = y[train_index].astype(str), y[test_index].astype(str)
+
+        X_vec_tr = vec.fit_transform(X_tr)
+        X_vec_test = vec.transform(X_test)
+        
+        clf.fit(X_vec_tr, y_tr)
+
+        y_pred_tr = clf.predict(X_vec_tr)
+        y_pred_test = clf.predict(X_vec_test)
+
+        train_recall_scores.append(recall_score(y_tr, y_pred_tr, pos_label='Negative emotion'))
+        train_precision_scores.append(precision_score(y_tr, y_pred_tr, pos_label='Negative emotion'))
+        train_f1_scores.append(f1_score(y_tr, y_pred_tr, pos_label='Negative emotion'))       
+        test_recall_scores.append(recall_score(y_test, y_pred_test, pos_label='Negative emotion'))
+        test_precision_scores.append(precision_score(y_test, y_pred_test, pos_label='Negative emotion'))
+        test_f1_scores.append(f1_score(y_test, y_pred_test, pos_label='Negative emotion'))       
+        
+        plot_confusion_matrix(clf, X_vec_test, y_test)
+        plt.title('Test set')
+        
+    print('Train mean recall:', round(pd.Series(train_recall_scores).mean(),2))
+    print('Train mean precision:', round(pd.Series(train_precision_scores).mean(),2))
+    print('Train mean F1:', round(pd.Series(train_f1_scores).mean(),2))
+    print('Test mean recall:', round(pd.Series(test_recall_scores).mean(),2))
+    print('Test mean precision:', round(pd.Series(test_precision_scores).mean(),2))
+    print('Test mean F1:', round(pd.Series(test_f1_scores).mean(),2))
